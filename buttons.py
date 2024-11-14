bfdd1e7608d97b6a5cd8f5c491ad4d573e321c77
N11CE1 <tomis.hickman@gmail.com>
2024-11-14 13:05:04 +1100

    buttons.py: > more comments for code explanation and removed unused imports

--- buttons.py
+++ buttons.py
@@ -1,17 +1,16 @@
-from PyQt5.QtWidgets import QLabel, QPushButton
-from PyQt5.QtGui import QFont, QColor
+from PyQt5.QtWidgets import QPushButton  # importing QPushButton to be the basis of our buttons
 from PyQt5.QtCore import Qt
 
 
 # defining toggle button (has on/off state)
-class ToggleButton(QPushButton): # taking QPushButton as an argument because I'm just altering the existing button type
+class ToggleButton(QPushButton):  # taking QPushButton as an argument because I'm just altering the existing button type
     def __init__(self, text, action=None):
         super().__init__(text)
         self.setCheckable(True)  # making button check-able aka stateful
-        self.setStyleSheet(self.false_style()) # set false style as default style
-        self.toggled.connect(self.update_style) # connects update style method to button presses
-        self.toggled.connect(self.execute_custom_action) # connects custom action to button presses
-        self.custom_action = action # custom action == action defined in __init__ which is None by default
+        self.setStyleSheet(self.false_style())  # set false style as default style
+        self.toggled.connect(self.update_style)  # connects update style method to button presses
+        self.toggled.connect(self.execute_custom_action)  # connects custom action to button presses
+        self.custom_action = action  # custom action == action defined in __init__ which is None by default
 
     # defining visual style of toggle button in off state to be returned
     # to the update style method
@@ -51,25 +50,25 @@ class ToggleButton(QPushButton): # taking QPushButton as an argument because I'm
 
     # executes a custom action based on the state of the button (checked = on)
     def execute_custom_action(self, checked):
-        if self.custom_action: # checks for existence of self.custom_action
-            self.custom_action(checked) # if self.custom_action exists, execute the custom action if button is checked
+        if self.custom_action:  # checks for existence of self.custom_action
+            self.custom_action(checked)  # if self.custom_action exists, execute the custom action if button is checked
 
 
 # defining sports button as a type of toggle button which takes 2 arguments
 # (the text it will display and the action it takes)
 def sports_button(text, action):
     s_button = ToggleButton(text, action)
-    s_button.setFixedSize(400, 150) # defining the size of the sports button
-    return s_button # returns the s_button object to the caller
+    s_button.setFixedSize(400, 150)  # defining the size of the sports button
+    return s_button  # returns the s_button object to the caller
 
 
 # defining standard push button
 # push_button is not a class because the QPushButton class already does everything we need it to
-# we're just defining an object of the QPushButton class and customising it's look for reusability
-def push_button(text, action): # takes text and action arguments
+# we're just defining an object of the QPushButton class and customising its look for reusability
+def push_button(text, action):  # takes text and action arguments
     p_button = QPushButton(text)
-    p_button.clicked.connect(action) # performs action on click
-    p_button.setFixedSize(200, 100) # setting dimensions
+    p_button.clicked.connect(action)  # performs action on click
+    p_button.setFixedSize(200, 100)  # setting dimensions
     p_button.setStyleSheet("""
         color: black;
         background-color: #F5F5F5;
@@ -77,5 +76,5 @@ def push_button(text, action): # takes text and action arguments
         font-size: 30px;
         border: 2px solid #D9D9D9;
         border-radius: 15px;
-        """) # looks stuff all set in style sheet
-    return p_button # returns p_button object to caller
+        """)  # looks stuff all set in style sheet
+    return p_button  # returns p_button object to caller
