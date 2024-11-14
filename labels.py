3eec16c5bc3e6d0b94a24a88bcc43b443eb00829
N11CE1 <tomis.hickman@gmail.com>
2024-11-14 13:03:23 +1100

    labels.py: >added comments to explain the code

--- labels.py
+++ labels.py
@@ -1,13 +1,14 @@
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel  # importing QLabel to be the basis of our labels
+from PyQt5.QtCore import Qt  # importing Qt for alignment abilities
 
 
-def large_text_label(text):
-    label = QLabel(text)
-    label.setAlignment(Qt.AlignCenter)
+# creating a default large test label to be reused wherever needed
+def large_text_label(text):  # takes argument of text for easy customisation
+    label = QLabel(text)  # our label will be an object of the pre-configured class QLabel
+    label.setAlignment(Qt.AlignCenter)  # setting text alignment so label contents are always centered
     label.setStyleSheet("""
                 font-helvetica;
                 font-size: 40px;
                 color: black;
-                """)
-    return label
+                """)  # using style sheet to set visual properties
+    return label  # returning our label to the caller
